32de8ba9b607338827206db172b26175a0a507b9
Asya <asyakazieva@mail.ru>
2022-04-17 06:58:34 +0300

    HW6

--- less6/js/CartComponent.js
+++ less6/js/CartComponent.js
@@ -7,4 +7,22 @@ Vue.component('cart', {
         </div>
     `
 });
-Vue.component()
+Vue.component('cart-item', {
+    props: ['img', 'cartItem'],
+    template: `
+    <div class="cart-item">
+                    <div class="product-bio">
+                        <img :src="img" alt="Some img">
+                        <div class="product-desc">
+                            <div class="product-title">{{ cartItem.product_name }}</div>
+                            <div class="product-quantity">Quantity: {{ cartItem.quantity }}</div>
+                            <div class="product-single-price">$ {{ cartItem.price }} each</div>
+                        </div>
+                    </div>
+                    <div class="right-block">
+                        <div class="product-price">{{cartItem.quantity*cartItem.price}}</div>
+                        <button class="del-btn" @click="$root.remove(cartItem)">&times;</button>
+                    </div>
+                </div>
+    `
+})
