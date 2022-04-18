e27a1634bd2bb9908a6b18caa67979c6166669ba
Asya <asyakazieva@mail.ru>
2022-04-18 19:30:58 +0300

    hw7

--- less7/project_express_single/server/server.js
+++ less7/project_express_single/server/server.js
@@ -56,23 +56,7 @@ app.put('/api/cart/:id', (req, res)=>{//пишем логику
     });
 });
 
-//delete
-app.delete('/api/cart/:id', (req, res)=>{//пишем логику
-    fs.readFile('./server/db/userCart.json', 'utf-8', (err, data)=>{
-        if (err) res.send(JSON.stringify({result: 0, err}));
-        else {
-            const cart = JSON.parse(data);
-            const find = cart.contents.find(good => good.id_product === Number(req.params.id));
 
-            find.quantity -= req.body.quantity;
-
-            fs.writeFile('./server/db/userCart.json', JSON.stringify(cart), (err)=> {
-                if (err) res.end(JSON.stringify({result: 0, err}));
-                else res.end(JSON.stringify({result: 1}));
-            });
-        }
-    });
-});
 
 
 
